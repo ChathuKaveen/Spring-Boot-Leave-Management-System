c034a859d762c10da70a098f3465676693817311
ChathuKaveen <chathukavinda12@gmail.com>
2026-02-26 22:12:23 +0530

    little changes to docker files

--- src/main/resources/db/migration/V2__making_leavedaytypetable.sql
+++ src/main/resources/db/migration/V2__making_leavedaytypetable.sql
@@ -15,7 +15,7 @@ CREATE TABLE `leave_date_type` (
     ALTER TABLE `leaves`
     ADD INDEX `from_date_foreign_idx` (`from_date_type` ASC) VISIBLE,
     ADD INDEX `to_date_foreign_idx` (`to_date_type` ASC) VISIBLE;
-    ;
+
     ALTER TABLE `leaves`
     ADD CONSTRAINT `from_date_foreign`
       FOREIGN KEY (`from_date_type`)
@@ -30,7 +30,7 @@ CREATE TABLE `leave_date_type` (
 
 ALTER TABLE `leaves`
 ADD INDEX `leave_type_id_idx` (`leave_type_id` ASC) VISIBLE;
-;
+
 ALTER TABLE `leaves`
 ADD CONSTRAINT `leave_type_id`
   FOREIGN KEY (`leave_type_id`)
